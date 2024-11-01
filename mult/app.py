42fb2f8f4f3e86562058c6ad4521ce1f504893d9
Pedro Henrique Alves da Silva <122069928+Phads@users.noreply.github.com>
2024-11-01 08:20:51 -0300

    Update app.py

--- mult/app.py
+++ mult/app.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from flask import Flask
+from flask import request
+app = Flask(__name__)
 
-app = FastAPI()
+@app.route("/mult", methods=["GET"])
+def soma():
+    res = {}
+    op1 = request.args.get('op1')
+    op2 = request.args.get('op2')
 
-class OperationInputs(BaseModel):
-    op1: float
-    op2: float
+    if not op1 or not op2:
+        res['resultado'] = "op1 ou op2 não informado"
+        return res, 400
 
-@app.get("/mult")
-def soma(op1: float, op2: float):
-    if op1 is None or op2 is None:
-        raise HTTPException(status_code=400, detail="op1 ou op2 não informado")
-    result = op1 * op2
-    return {"resultado": result}
+    res['resultado'] = float(op1) * float(op2)
+    return res, 200
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
